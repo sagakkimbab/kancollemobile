b2629b18f42b799933511ae4ce7e43e1ac865527
ddugi1214 <ddugi1214@naver.com>
2015-05-30 14:11:29 +0900

    cache err fix

---
+++ app/src/main/java/com/ddugi1214/kancolle/MainActivity.java
@@ -0,0 +1,30 @@
+package com.ddugi1214.kancolle;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+
+public class MainActivity extends ActionBarActivity {
+    WebView mWeb;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWeb = (WebView)findViewById(R.id.web);
+        WebSettings set = mWeb.getSettings();
+        set.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        set.setJavaScriptEnabled(true);
+        set.setSupportZoom(true);
+        mWeb.loadUrl("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
