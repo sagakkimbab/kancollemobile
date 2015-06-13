db1bf048bcc8d7717b1e685de689cd9faadca280
ddugi1214 <ddugi1214@naver.com>
2015-06-13 10:59:28 +0900

    added finish asking, fix some err

--- app/src/main/java/com/ddugi1214/kancolle/MainActivity.java
+++ app/src/main/java/com/ddugi1214/kancolle/MainActivity.java
@@ -1,11 +1,17 @@
 package com.ddugi1214.kancolle;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -15,7 +21,20 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         mWeb = (WebView)findViewById(R.id.web);
+        mWeb.setWebChromeClient(new WebChromeClient() {
+             ProgressBar probar = (ProgressBar)findViewById(R.id.ProgressBar);
+
+             public void onProgressChanged(WebView view,int progress) {
+                 probar.setProgress(progress);
+                 if (progress == 100) {
+                     probar.setVisibility(View.GONE);
+                 } else {
+                     probar.setVisibility(View.VISIBLE);
+                 }
+             }
+        });
         WebSettings set = mWeb.getSettings();
+        set.setUserAgentString("Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0; SLCC2;.NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)");
         set.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
         set.setJavaScriptEnabled(true);
         set.setSupportZoom(true);
@@ -27,6 +46,27 @@ public class MainActivity extends ActionBarActivity {
             view.loadUrl(url);
             return true;
         }
+    }
+
+    @Override
+    public boolean onKeyDown(int KeyCode, KeyEvent event) {
+        super.onKeyDown(KeyCode, event);
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+        switch (KeyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                new AlertDialog.Builder(this)
+                .setTitle("칸코레 모바일 접속기")
+                .setMessage("칸코레 모바일 접속기를 종료합니다")
+                .setPositiveButton("종료", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("취소", null)
+                .show();
+        }
+        }
+        return true;
     }
 
     @Override
