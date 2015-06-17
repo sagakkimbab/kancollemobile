c2aa16fd87d03d8f580fe67a1354e8aa11a8c6ad
ddugi1214 <ddugi1214@naver.com>
2015-06-17 17:41:26 +0900

    fix err, add pinch zoom

--- app/src/main/java/com/ddugi1214/kancolle/MainActivity.java
+++ app/src/main/java/com/ddugi1214/kancolle/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -15,6 +16,11 @@ import android.widget.ProgressBar;
 
 
 public class MainActivity extends ActionBarActivity {
+    //추후 수정
+    //final static float STEP = 200;
+    //float mRatio = 1.0f;
+    //int mBaseDist;
+    //float mBaseRatio;
     WebView mWeb;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,24 +28,57 @@ public class MainActivity extends ActionBarActivity {
 
         mWeb = (WebView)findViewById(R.id.web);
         mWeb.setWebChromeClient(new WebChromeClient() {
-             ProgressBar probar = (ProgressBar)findViewById(R.id.ProgressBar);
+            ProgressBar probar = (ProgressBar) findViewById(R.id.ProgressBar);
 
-             public void onProgressChanged(WebView view,int progress) {
-                 probar.setProgress(progress);
-                 if (progress == 100) {
-                     probar.setVisibility(View.GONE);
-                 } else {
-                     probar.setVisibility(View.VISIBLE);
-                 }
-             }
+            public void onProgressChanged(WebView view, int progress) {
+                probar.setProgress(progress);
+                if (progress == 100) {
+                    probar.setVisibility(View.GONE);
+                } else {
+                    probar.setVisibility(View.VISIBLE);
+                }
+            }
         });
         WebSettings set = mWeb.getSettings();
         set.setUserAgentString("Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0; SLCC2;.NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)");
         set.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
         set.setJavaScriptEnabled(true);
+        set.setJavaScriptCanOpenWindowsAutomatically(true);
         set.setSupportZoom(true);
+
+        CookieManager cookiemanager = CookieManager.getInstance();
+        cookiemanager.setAcceptCookie(true);
+        cookiemanager.hasCookies();
+
         mWeb.loadUrl("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/");
     }
+
+    //이하 오류 발생, 수정중
+    //public Boolean onTouchEvent(MotionEvent event) {
+        //if (event.getPointerCount() == 2) {
+            //int action = event.getAction();
+            //int pureaction = action & MotionEvent.ACTION_MASK;
+            //if (pureaction == MotionEvent.ACTION_POINTER_DOWN) {
+                //mBaseDist = getDistance(event);
+                //mBaseRatio = mRatio;
+            //} else {
+                //float delta = (getDistance(event) - mBaseDist) / STEP;
+                //float multi = (float)Math.pow(2, delta);
+                //mRatio = Math.min(1024.0f, Math.max(0.1f, mBaseRatio * multi));
+
+                //Matrix m = new Matrix();
+                //m.postScale(mRatio, mRatio);
+                //mWeb.setWebViewMatrix(m);
+            //}
+        //}
+        //return true;
+    //}
+
+    //int getDistance(MotionEvent event) {
+        //int dx = (int)(event.getX(0) - event.getX(1));
+        //int dy = (int)(event.getY(0) - event.getY(1));
+        //return (int)(Math.sqrt(dx * dx + dy * dy));
+    //}
 
     class MyWebClient extends WebViewClient {
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
